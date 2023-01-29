4dbe8534357db9c0ec01034d419e55e0a1a1be42
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-01-29 20:50:03 -0300

    Ajuste no layout principal e feitos dos elementos

--- src/components/InfosGerais/InfosGerais.jsx
+++ src/components/InfosGerais/InfosGerais.jsx
@@ -13,51 +13,78 @@ function InfosGerais() {
 
             <div className="infos-cards">
             
-                <div className="infos-card">
-                    <div className="infos-basic">
-                        <img className="card-icon" src={personIcon} alt="person icon" />
-                        <div className="porcentagem-box-green">
-                            <p className="porcentagem-text">+30%</p>
+                <div className="infos-card-main">
+                    <div className="infos-card">
+                        <div className="infos-basic">
+                            <img className="card-icon" src={personIcon} alt="person icon" />
+                            <div className="porcentagem-box-green">
+                                <p className="porcentagem-text">+30%</p>
+                            </div>
                         </div>
+                        <p className="principal-number">26</p>
+                        <p className="principal-text">Pacientes ativos</p>
                     </div>
-                    <p className="principal-number">26</p>
-                    <p className="principal-text">Pacientes ativos</p>
+                    <div className="up-menu">
+                        <p className="sub-box-title">Pacientes totais</p>
+                        <p className="sub-box-subtitle">268</p>
+                    </div>
+                    <div className="back-white-box"></div>
+                </div>
+                
+                <div className="infos-card-main">
+                    <div className="infos-card">
+                        <div className="infos-basic">
+                            <img className="card-icon" src={testeIcon} alt="teste icon" />
+                            <div className="porcentagem-box-red">
+                                <p className="porcentagem-text">-10%</p>
+                            </div>
+                        </div>
+                        <p className="principal-number">306</p>
+                        <p className="principal-text">Testes realizados</p>
+                    </div>
+                    <div className="up-menu">
+                        <p className="sub-box-title">Testes totais</p>
+                        <p className="sub-box-subtitle">1.265</p>
+                    </div>
+                    <div className="back-white-box"></div>
                 </div>
 
-                <div className="infos-card">
-                    <div className="infos-basic">
-                        <img className="card-icon" src={testeIcon} alt="teste icon" />
-                        <div className="porcentagem-box-red">
-                            <p className="porcentagem-text">-10%</p>
+                <div className="infos-card-main">
+                    <div className="infos-card">
+                        <div className="infos-basic">
+                            <img className="card-icon" src={testeOkIcon} alt="person icon" />
+                            <div className="porcentagem-box-green">
+                                <p className="porcentagem-text">+30%</p>
+                            </div>
                         </div>
+                        <p className="principal-number">139</p>
+                        <p className="principal-text">Testes concluídos</p>
                     </div>
-                    <p className="principal-number">306</p>
-                    <p className="principal-text">Testes realizados</p>
+                    <div className="up-menu-big">
+                        <p className="sub-box-title">Testes totais concluídos</p>
+                        <p className="sub-box-subtitle">10.550</p>
+                    </div>
+                    <div className="back-white-box"></div>
                 </div>
-
-                <div className="infos-card">
-                    <div className="infos-basic">
-                        <img className="card-icon" src={testeOkIcon} alt="person icon" />
-                        <div className="porcentagem-box-green">
-                            <p className="porcentagem-text">+30%</p>
+                
+                <div className="infos-card-main">
+                    <div className="infos-card">
+                        <div className="infos-basic">
+                            <img className="card-icon" src={testeCancelIcon} alt="person icon" />
+                            <div className="porcentagem-box-red">
+                                <p className="porcentagem-text">-3%</p>
+                            </div>
                         </div>
+                        <p className="principal-number">11</p>
+                        <p className="principal-text">Testes cancelados</p>
                     </div>
-                    <p className="principal-number">139</p>
-                    <p className="principal-text">Testes concluídos</p>
+                    <div className="up-menu-big">
+                        <p className="sub-box-title">Testes totais cancelados</p>
+                        <p className="sub-box-subtitle-red">618</p>
+                    </div>
+                    <div className="back-white-box"></div>
                 </div>
-
-                <div className="infos-card">
-                    <div className="infos-basic">
-                        <img className="card-icon" src={testeCancelIcon} alt="person icon" />
-                        <div className="porcentagem-box-red">
-                            <p className="porcentagem-text">-3%</p>
-                        </div>
-                    </div>
-                    <p className="principal-number">11</p>
-                    <p className="principal-text">Testes cancelados</p>
-                    </div>
-                </div>
-
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './Navbar.css'
-import lupa from '../../assets/imgs/lupa.png'
-import settings from '../../assets/imgs/settings.png'
-import notification from '../../assets/imgs/bell.png'
-import perfilPhoto from '../../assets/imgs/perfil-photo.png' 
-import appsIcon from '../../assets/imgs/apps-icon.png'
+import './Navbar.css';
+import lupa from '../../assets/imgs/lupa.png';
+import settings from '../../assets/imgs/settings.png';
+import notification from '../../assets/imgs/bell.png';
+import perfilPhoto from '../../assets/imgs/perfil-photo.png';
+import appsIcon from '../../assets/imgs/apps-icon.png';
+import sairIcon from '../../assets/imgs/sair.png';
 
 function Navbar() {
     return (
@@ -21,8 +22,18 @@ function Navbar() {
             </div>
 
             <div className="perfil">
-                <p className="perfil-text1">Olá,</p><p className="perfil-text2">Felipe</p>
-                <img className="perfil-photo" src={perfilPhoto} alt="perfil photo" />
+                <button className="btn-perfil">
+                    <p className="perfil-text1">Olá,</p><p className="perfil-text2">Felipe</p>
+                    <img className="perfil-photo" src={perfilPhoto} alt="perfil photo" />
+                </button>
+                <div className="dropdown-menu">
+                    <a href="#">Minha conta</a>
+                    <a className="borders-menu" href="#">Configurações</a>
+                    <div className="logout-box">
+                        <img className="logout-icon" src={sairIcon} alt="icone logout" />
+                        <a className="logout-text" href="#">Sair</a>
+                    </div>
+                </div>
             </div>
 
             <div className="apps">
